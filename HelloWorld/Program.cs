fb822f4363ce3573b4878205285c700d68bd1a68
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2023-09-12 23:43:17 +0800

    changed the measurement to: 0 to 60 => Normal Rainfall 61 to 80 => Heavy Rainfall 81 and above => Thurnderstorm

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -3,24 +3,27 @@ bool question = Convert.ToBoolean(Console.ReadLine());
 
 var isRainyDay = (question == true) ? "Yes, It's Rainy" : "No, It's Sunny";
 
-int rainGauged = 0;
 
 if (question == true)
 {
+    Console.WriteLine($"{isRainyDay}\n");
     Console.WriteLine("Enter amount of rain gauged(cm) :");
-    rainGauged = Convert.ToInt32(Console.ReadLine());
+    int rainGauged = Convert.ToInt32(Console.ReadLine());
 
     // to checked gauged rain
-    // if above 60 is normal rainfall
-    // above 80 is heavy rainfall
-    // above 100 is thurnderstorm
+    // if 0 - 60 is normal rainfall
+    // 61 - 80 is heavy rainfall
+    // 81 and above is thurnderstorm
 
-    if (rainGauged < 60)
+    if (rainGauged <= 60)
     {
         Console.WriteLine("It's a normal rainfall.");
-    }else if (rainGauged )
+    }else if (rainGauged <= 80)
     {
-
+        Console.WriteLine("Ooh no, it's a heavy rainfall");
+    }else if (rainGauged > 81)
+    {
+        Console.WriteLine("You can't go outside. stay put it's a Thurnderstorm.");
     }
 }else
 {
