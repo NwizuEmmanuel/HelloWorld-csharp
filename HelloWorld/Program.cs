ab41f584a17e27b49800f58f5d7185752b2f7ab9
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2023-09-14 20:32:24 +0800

    playing with classes

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,7 +2,7 @@
 {
     public static void Main(string[] args)
     {
-        GreetPerson("John");
+        Console.WriteLine($"Addition: {AddNumbers(50, 40)}");
     }
 
     static void SayHello()
@@ -13,5 +13,30 @@
     static void GreetPerson(string firstname)
     {
         Console.WriteLine($"Hello, {firstname}");
+    }
+
+    static void DisplayUserInfo(string firstname, string lastname, int age)
+    {
+        Console.WriteLine($"Fullname: {firstname} {lastname}\nAge: {age}");
+    }
+
+    static void WelcomeMessage(string name = "John Doe")
+    {
+        Console.WriteLine($"Welcome to PizzaHut, {name}");
+    }
+
+    static int AreaOfSquare(int width, int height)
+    {
+        return width * height;
+    }
+
+    static int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+
+    static double AddNumbers(double a, double b)
+    {
+        return a + b;
     }
 }
